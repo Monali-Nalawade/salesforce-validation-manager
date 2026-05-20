13d3beb2cfb437943962a54969e9188fc4136608
Monali Ketu Patil <156786303+Monali-Nalawade@users.noreply.github.com>
2026-05-20 16:25:57 +0530

    Update LoginPage.js

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,37 +1,46 @@
 import React from "react";
-import {Box, Paper,Typography,  Button} from "@mui/material";
+
+import {
+    Box,
+    Paper,
+    Typography,
+    Button
+} from "@mui/material";
 
 const LoginPage = () => {
+
     const handleLogin = () => {
 
         window.location.href =
-    "https://sfswitch-4tts.onrender.com/auth/login";
+            "https://sfswitch-4tts.onrender.com/auth/login";
     };
 
     return (
 
-<Box
-    sx={{
-        minHeight: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        background:
-            "linear-gradient(135deg, #0f172a, #1e293b)"
-    }}
->
+        <Box
+            sx={{
+                minHeight: "100vh",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                background:
+                    "linear-gradient(135deg, #0f172a, #1e293b)"
+            }}
+        >
 
             <Paper
                 elevation={3}
-    sx={{
-    p: 5,
-    borderRadius: 4,
-    width: 400,
-    textAlign: "center",
-    backgroundColor: "#1e293b",
-    color: "white",
-    boxShadow: "0px 0px 20px rgba(0,0,0,0.4)"
-}}
+                sx={{
+                    p: 5,
+                    borderRadius: 4,
+                    width: 400,
+                    textAlign: "center",
+                    backgroundColor: "#1e293b",
+                    color: "white",
+                    boxShadow:
+                        "0px 0px 20px rgba(0,0,0,0.4)"
+                }}
+            >
 
                 <Typography
                     variant="h4"
@@ -50,16 +59,16 @@ const LoginPage = () => {
                     size="large"
                     fullWidth
                     onClick={handleLogin}
-                sx={{
-               mt: 2,
-               backgroundColor: "#2563eb",
-              "&:hover": {
-              backgroundColor: "#1d4ed8"
-                }
-             }}
-             >
-    Login with Salesforce
-</Button>
+                    sx={{
+                        mt: 2,
+                        backgroundColor: "#2563eb",
+                        "&:hover": {
+                            backgroundColor: "#1d4ed8"
+                        }
+                    }}
+                >
+                    Login with Salesforce
+                </Button>
 
             </Paper>
 
